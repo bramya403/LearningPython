ea92d8b25f539a7920fbbb4a80ea9ed2c0117da2
Bandaru Ramya <bramya403@gmail.com>
2021-03-29 21:02:10 +0530

    Create samples.py
    My first program

---
+++ basics/samples.py
@@ -0,0 +1,17 @@
+# Simple python examples
+
+a = 22
+print(a)
+a = "Some random string"
+print(a)
+a = 22.7
+print(a)
+
+a = [1, 2, 100, 4, 5, "s", "abcd"]
+print(a)
+print(len(a))
+
+b = [2, 3, 4]
+
+a = a + b
+print(a)
